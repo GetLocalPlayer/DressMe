347ce229d52dfec624313042cc4d99381e158a7c
GetLocalPlayer <GetLocalPlayer@gmail.com>
2020-11-09 16:59:34 +0400

    Swapped buttons in Wowhead dialog

--- DressMe.lua
+++ DressMe.lua
@@ -144,8 +144,8 @@ end)
 
 StaticPopupDialogs["DRESSME_WOWHEAD_URL_DIALOG"] = {
     text = "DRESSME_WOWHEAD_URL_DIALOG",
-    button1 = ACCEPT,
-    button2 = "Version",
+    button1 = "Version",
+    button2 = CLOSE,
     timeout = 0,
     whileDead = true,
     hideOnEscape = true,
@@ -165,16 +165,16 @@ local function showWowheadURLDialog(itemId)
         self.text:SetText(("Wowhead \124cff00ff00%s\124r"):format(isRetail and "Retail" or "Classic"))
         self.wideEditBox:SetText(("https://%s.wowhead.com/item=%s"):format((isRetail and "www" or "classic"), itemId))
         self.wideEditBox:HighlightText()
-        self.button2:SetText(isRetail and "Classic" or "Retail")
+        self.button1:SetText(isRetail and "Classic" or "Retail")
     end
 
-    StaticPopupDialogs["DRESSME_WOWHEAD_URL_DIALOG"].OnCancel = function(self)
+    StaticPopupDialogs["DRESSME_WOWHEAD_URL_DIALOG"].OnAccept = function(self)
         isRetail = not isRetail
         isCanceled = true
         StaticPopup_Hide("DRESSME_WOWHEAD_URL_DIALOG")
     end
 
-    StaticPopupDialogs["DRESSME_WOWHEAD_URL_DIALOG"].OnAccept = function(self)
+    StaticPopupDialogs["DRESSME_WOWHEAD_URL_DIALOG"].OnCancel = function(self)
         isCanceled = false
     end
 
